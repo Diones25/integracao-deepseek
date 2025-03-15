36f1a40829fc32b55c6a0bc8d02b5efe434af734
Diones <pereiradiones987@gmail.com>
2025-03-15 14:38:06 -0300

    notações do swagger na controller

--- src/deep-seek/deep-seek.controller.ts
+++ src/deep-seek/deep-seek.controller.ts
@@ -1,12 +1,33 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { DeepSeekService } from './deep-seek.service';
 import { PromptDeepSeekDto } from './dtos/prompt-deepseek.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('chat')
 export class DeepSeekController {
   constructor(private readonly deepSeekService: DeepSeekService) { }
   
   @Post('send')
+  @ApiOperation({
+    summary: 'Pergunta ao DeepSeek',
+    description: 'Endpoint para perguntar ao DeepSeek'
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Resposta do DeepSeek',
+    example: {
+      response: 'A capital da Alemanha é Berlim'
+    }
+  })
+  @ApiResponse({
+    status: 400,
+    description: "Falha na requisição ao DeepSeek",
+    example: {
+      message: "Failed to generate response from DeepSeek",
+      error: "Bad Request",
+      statusCode: 400
+    }
+  })
   async sendMessage(@Body() promptDeepSeekDto: PromptDeepSeekDto): Promise<{ response: string }> {
     const response = await this.deepSeekService.generateResponse(promptDeepSeekDto.prompt);
     return { response };
