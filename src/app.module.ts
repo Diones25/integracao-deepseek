1ab91ffd4297e186fd50aa38b3439d1cf18297be
Diones <pereiradiones987@gmail.com>
2025-03-15 13:28:03 -0300

    configurando variaveis globais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DeepSeekModule } from './deep-seek/deep-seek.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DeepSeekModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DeepSeekModule
+  ],
   controllers: [],
   providers: [],
 })
