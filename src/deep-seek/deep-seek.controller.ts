d29332783da4acafb957b60e8125f2d9e6911b85
Diones <pereiradiones987@gmail.com>
2025-03-15 14:10:52 -0300

    criando controller

--- src/deep-seek/deep-seek.controller.ts
+++ src/deep-seek/deep-seek.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { DeepSeekService } from './deep-seek.service';
+import { PromptDeepSeekDto } from './dtos/prompt-deepseek.dto';
 
-@Controller('deep-seek')
-export class DeepSeekController {}
+@Controller('chat')
+export class DeepSeekController {
+  constructor(private readonly deepSeekService: DeepSeekService) { }
+  
+  @Post('send')
+  async sendMessage(@Body() promptDeepSeekDto: PromptDeepSeekDto): Promise<{ response: string }> {
+    const response = await this.deepSeekService.generateResponse(promptDeepSeekDto.prompt);
+    return { response };
+  }
+}
