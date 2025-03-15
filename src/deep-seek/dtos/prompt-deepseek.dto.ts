9a9617040a691aacb10b84a8780e9fbab4262f17
Diones <pereiradiones987@gmail.com>
2025-03-15 14:35:22 -0300

    notação no dto

--- src/deep-seek/dtos/prompt-deepseek.dto.ts
+++ src/deep-seek/dtos/prompt-deepseek.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class PromptDeepSeekDto {
 
+  @ApiProperty({ example: 'Qual a capital da Alemanha', description: 'Prompt para o DeepSeek' })
   @IsString({ message: 'Deve ser uma string' })
   prompt: string;
 }
